ce990cc26dfac54618408e402687ed433570fa9c
Graciela Parente <graciparentte@gmail.com>
2022-04-26 18:45:52 -0300

    criação da pasta infraestruturas e atualização de arquivos

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const consign = require("consign");
-const app = express();
+const bodyParser = require("body-parser");
 
-consign().include("controllers").into(app);
+module.exports = () => {
+  const app = express();
 
-app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  consign().include("controllers").into(app);
+  return app;
+};
---
+++ index.js
@@ -0,0 +1,12 @@
+const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/conexao");
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("conectado com sucesso");
+
+    const app = customExpress();
+    app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+  }
+});
